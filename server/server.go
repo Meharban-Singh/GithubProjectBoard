31d2ca9260f88cc88506e9e9bab5bf91e67e0a47
Meharban-Singh <meharbansingh10.ms@gmail.com>
2020-07-12 08:43:23 -0500

    Added another URL route to list a single project details, refactoring code

--- server/server.go
+++ server/server.go
@@ -38,65 +38,9 @@ func getAccessToken() {
 	accessToken = config.PAT
 }
 
-// Handles login requests - stores access token
-// URL '/login'
-// Returns status codes 400 or 401
-func loginHandler(c echo.Context) error {
-	getAccessToken();
-	
-	// If got status code, return 400 else return 401
-	if accessToken != "" {
-		return c.String(http.StatusOK, "SUCCESS");
-	} else {
-		return c.String(http.StatusUnauthorized, "REJECTED")
-	}
-}
-
-// Returns a list of repo's for authenticated user
-// URL: /repos
-// Returns status codes 400 or 500 for errors
-func getAllRepos(c echo.Context) error {
+func sendGETReqToGH(url string, c echo.Context) error {
 	//Get HTTP client 
 	client := &http.Client{}
-	
-	//Create GET request object
-	req, err := http.NewRequest("GET", "https://api.github.com/user/repos", nil)
-	if err != nil {
-		log.Fatal(err)
-		return c.String(http.StatusInternalServerError, "FATAL: Cannot access GH account!");
-	}
-	
-	//Set Authorization token header
-	req.Header.Set("Authorization", "token " + accessToken)
-	
-	//Send request
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-		return c.String(http.StatusInternalServerError, "FATAL: Cannot access GH account!");
-	}
-	
-	//Read response and send it 
-	data, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-		return c.String(http.StatusInternalServerError, "FATAL: Cannot read response from GH!");
-	}
-	
-	// All good now, send 400
-	return c.String(http.StatusOK, string(data))
-}
-
-// Returns all projects of some repo
-// User must be authenticated before
-// URL: /repos/:ownerOfRepo/:repoName/projects
-// Returns status codes 400 or 500
-func getProjectsOfRepo(c echo.Context) error {
-	//Get HTTP client 
-	client := &http.Client{}
-	
-	var url =  "https://api.github.com/repos/" + c.Param("user") + "/" + c.Param("repo") + "/projects"
 
 	//Create GET request object
 	req, err := http.NewRequest("GET", url, nil)
@@ -128,6 +72,38 @@ func getProjectsOfRepo(c echo.Context) error {
 	return c.String(http.StatusOK, string(data))
 }
 
+// Handles login requests - stores access token
+// URL '/login'
+// Returns status codes 400 or 401
+func loginHandler(c echo.Context) error {
+	getAccessToken();
+	
+	// If got status code, return 400 else return 401
+	if accessToken != "" {
+		return c.String(http.StatusOK, "SUCCESS");
+	} else {
+		return c.String(http.StatusUnauthorized, "REJECTED")
+	}
+}
+
+// Returns a list of repo's for authenticated user
+// URL: /repos
+// Returns status codes 400 or 500 for errors
+func getAllRepos(c echo.Context) error {
+	return sendGETReqToGH("https://api.github.com/user/repos", c)
+}
+
+// Returns all projects of some repo
+// User must be authenticated before
+// URL: /repos/:ownerOfRepo/:repoName/projects
+// Returns status codes 400 or 500
+func getProjectsOfRepo(c echo.Context) error {
+	return sendGETReqToGH("https://api.github.com/repos/" + c.Param("user") + "/" + c.Param("repo") + "/projects", c)
+}
+
+func getProjectDetails(c echo.Context) error {
+	return sendGETReqToGH("https://api.github.com/projects/" + c.Param("projectID"), c)
+}
 
 // Main function
 func main() {
@@ -137,6 +113,7 @@ func main() {
 	// Routes
 	app.GET("/login", loginHandler)
 	app.GET("/repos/:user/:repo/projects", getProjectsOfRepo)
+	app.GET("/projects/:projectID", getProjectDetails)
 	app.GET("/repos", getAllRepos)
 
 	//Start server
